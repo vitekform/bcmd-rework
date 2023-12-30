7d1c5297488999f5110311e20b3309cffb45da97
Vít Formánek <vit.formanek.2022b@gypce.cz>
2023-12-30 14:07:26 +0000

    1.7 upload

---
+++ versions/1.7/src/main/java/cz/vitekform/bcmdreborn/cmds/gmsp.java
@@ -0,0 +1,107 @@
+package cz.vitekform.bcmdreborn.cmds;
+
+import cz.vitekform.bcmdreborn.functions.configfunctions;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.logging.Level;
+
+public class gmsp implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        String lang = configfunctions.get().getString("lang");
+        if (lang.equalsIgnoreCase("cs_cz")){
+            if (sender instanceof Player){
+                Player p = (Player) sender;
+                if (sender.hasPermission("bcmd.gamemode") || sender.hasPermission("bcmd.admin")){
+                    if (args.length != 1){
+                        p.setGameMode(GameMode.SPECTATOR);
+                        p.sendMessage(ChatColor.GREEN + "Nastavil jsi si gamemode na Spectator!");
+                    }
+                    else {
+                        if (Bukkit.getOnlinePlayers().contains(Bukkit.getPlayer(args[0]))){
+                            Player target = Bukkit.getPlayer(args[0]);
+                            target.setGameMode(GameMode.SPECTATOR);
+                            target.sendMessage(ChatColor.GREEN + p.getName() + " ti nastavil gamemode Spectator!");
+                            p.sendMessage(ChatColor.GREEN + "Nastavil jsi gamemode Spectator hráči " + target.getName());
+                        }
+                        else {
+                            p.sendMessage(ChatColor.RED + "Hráč nebyl nalezen!");
+                        }
+                    }
+                }
+                else {
+                    p.sendMessage(ChatColor.RED + "Na toto nemáš práva!\n" + ChatColor.GRAY + "*Takže žádnej gmsp*");
+                    p.playSound(p, Sound.ENTITY_VILLAGER_NO, 100, 100);
+                }
+            }
+            else {
+                if (args.length != 1){
+                    Bukkit.getLogger().log(Level.INFO, ChatColor.RED + "Pokud tento příkaz používá konzole, musí přidat 1 argument!");
+                }
+                else {
+                    if (Bukkit.getOnlinePlayers().contains(Bukkit.getPlayer(args[0]))){
+                        Player target = Bukkit.getPlayer(args[0]);
+                        target.setGameMode(GameMode.SPECTATOR);
+                        target.sendMessage(ChatColor.GREEN + "Konzole ti nastavila gamemode Spectator!");
+                        Bukkit.getLogger().log(Level.INFO, ChatColor.GREEN + "Nastavil jsi hráči " + target.getName() + " gamemode Spectator!");
+                    }
+                    else {
+                        Bukkit.getLogger().log(Level.INFO, ChatColor.RED + "Hráč nebyl nalezen!");
+                    }
+                }
+            }
+        }
+        else if (lang.equalsIgnoreCase("en_us")){
+            if (sender instanceof Player){
+                Player p = (Player) sender;
+                if (sender.hasPermission("bcmd.gamemode") || sender.hasPermission("bcmd.admin")){
+                    if (args.length != 1){
+                        p.setGameMode(GameMode.SPECTATOR);
+                        p.sendMessage(ChatColor.GREEN + "You have set your gamemode to Spectator!");
+                    }
+                    else {
+                        if (Bukkit.getOnlinePlayers().contains(Bukkit.getPlayer(args[0]))){
+                            Player target = Bukkit.getPlayer(args[0]);
+                            target.setGameMode(GameMode.SPECTATOR);
+                            target.sendMessage(ChatColor.GREEN + p.getName() + " set your gamemode to Spectator!");
+                            p.sendMessage(ChatColor.GREEN + "You have set gamemode Spectator to " + target.getName());
+                        }
+                        else {
+                            p.sendMessage(ChatColor.RED + "Player wasn't found!");
+                        }
+                    }
+                }
+                else {
+                    p.sendMessage(ChatColor.RED + "You don't have permission to do this!\n" + ChatColor.GRAY + "*So no gmsp*");
+                    p.playSound(p, Sound.ENTITY_VILLAGER_NO, 100, 100);
+                }
+            }
+            else {
+                if (args.length != 1){
+                    Bukkit.getLogger().log(Level.INFO, ChatColor.RED + "If console uses this command, it must add 1 argument!");
+                }
+                else {
+                    if (Bukkit.getOnlinePlayers().contains(Bukkit.getPlayer(args[0]))){
+                        Player target = Bukkit.getPlayer(args[0]);
+                        target.setGameMode(GameMode.SPECTATOR);
+                        target.sendMessage(ChatColor.GREEN + "Console set your gamemode to Spectator!");
+                        Bukkit.getLogger().log(Level.INFO, ChatColor.GREEN + "You have set gamemode Spectator to " + target.getName() + "!");
+                    }
+                    else {
+                        Bukkit.getLogger().log(Level.INFO, ChatColor.RED + "Player wasn't found!");
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
